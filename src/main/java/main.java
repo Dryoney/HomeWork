69af3dcac156b43a3d3d899bc6a0f3b431d040a5
dryoney <bitytski.aleks@gmail.com>
2023-03-28 23:56:35 +0300

    Reformat code

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,14 +6,14 @@ class Main {
         int n = sc.nextInt();
         int[][] mass1 = new int[n][n];
         int counter = 1;
-        for (int i = 0; i<n/2;i++){
-            for (int j = i;j<n-i;j++) mass1[i][j]=counter++;
-            for (int j = i+1;j<n-i;j++) mass1[j][n-1-i]=counter++;
-            for (int j = n-2-i;j>=i;j--) mass1[n-1-i][j]=counter++;
-            for (int j = n-2-i;j>i;j--) mass1[j][i]=counter++;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i; j++) mass1[i][j] = counter++;
+            for (int j = i + 1; j < n - i; j++) mass1[j][n - 1 - i] = counter++;
+            for (int j = n - 2 - i; j >= i; j--) mass1[n - 1 - i][j] = counter++;
+            for (int j = n - 2 - i; j > i; j--) mass1[j][i] = counter++;
         }
-        if(n%2==1)
-            mass1[n/2][n/2]=counter;
+        if (n % 2 == 1)
+            mass1[n / 2][n / 2] = counter;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(mass1[i][j] + " ");
